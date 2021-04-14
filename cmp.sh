87b9810792c508691644304709c61117393d9bcc
Michael Lore <mike.lore@sap.com>
2021-04-13 22:40:26 -0400

    compare published image too

--- cmp.sh
+++ cmp.sh
@@ -2,13 +2,17 @@
 
 docker run --name gcr gcr.io/distroless/static:nonroot /foo
 docker run --name goimg goimg:latest /foo
+docker run --name goimg2 ancientlore/goimg:latest /foo
 
 docker export gcr | tar -tv | cut -b "1-28,49-" > cmp-gcrio-distroless.txt
 docker export goimg | tar -tv | cut -b "1-28,49-" > cmp-goimg.txt
+docker export goimg2 | tar -tv | cut -b "1-28,49-" > cmp-goimg-pub.txt
 
 docker cp gcr:/etc/passwd gcr-passwd.txt
 docker cp gcr:/etc/group gcr-group.txt
 docker cp goimg:/etc/passwd goimg-passwd.txt
 docker cp goimg:/etc/group goimg-group.txt
+docker cp goimg2:/etc/passwd goimg-pub-passwd.txt
+docker cp goimg2:/etc/group goimg-pub-group.txt
 
-docker rm gcr goimg
+docker rm gcr goimg goimg2
