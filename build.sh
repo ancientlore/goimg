d6813aa3a8e1568078124c8f2fb78c55a1cc53a7
Michael Lore <michael.lore@gmail.com>
2023-06-16 15:16:09 -0400

    Confirm options before proceeding

--- build.sh
+++ build.sh
@@ -8,36 +8,40 @@ echo Go version is $GO_VERSION, major version is $VERSION
 
 echo
 echo Building ancientlore/goimg:$GO_VERSION
-docker build --build-arg GO_VERSION=$GO_VERSION -t ancientlore/goimg:$GO_VERSION . || return 1
+docker build --build-arg GO_VERSION=$GO_VERSION -t ancientlore/goimg:$GO_VERSION . || exit 1
 
 if [ "$GO_VERSION" != "$VERSION" ]; then
     echo
     echo Tagging ancientlore/goimg:$VERSION
-    docker tag ancientlore/goimg:$GO_VERSION ancientlore/goimg:$VERSION || return 1
+    docker tag ancientlore/goimg:$GO_VERSION ancientlore/goimg:$VERSION || exit 1
 fi
 
 echo
 echo Tagging ancientlore/goimg:latest
-docker tag ancientlore/goimg:$GO_VERSION ancientlore/goimg:latest || return 1
+docker tag ancientlore/goimg:$GO_VERSION ancientlore/goimg:latest || exit 1
+
+gum confirm "Test?" || exit 1
 
 echo
 echo Building test image goimgtest:$VERSION
-docker build --build-arg GO_VERSION=$GO_VERSION -t goimgtest:$VERSION -f Dockerfile.test . || return 1
+docker build --build-arg GO_VERSION=$GO_VERSION -t goimgtest:$VERSION -f Dockerfile.test . || exit 1
 
 echo
 echo Running test image goimgtest:$VERSION
-docker run -it --rm goimgtest:$VERSION || return 1
+docker run -it --rm goimgtest:$VERSION || exit 1
+
+gum confirm "Push?" || exit 1
 
 echo
 echo Pushing ancientlore/goimg:$GO_VERSION
-docker push ancientlore/goimg:$GO_VERSION
+docker push ancientlore/goimg:$GO_VERSION || exit 1
 
 if [ "$GO_VERSION" != "$VERSION" ]; then
     echo
     echo Pushing ancientlore/goimg:$VERSION 
-    docker push ancientlore/goimg:$VERSION
+    docker push ancientlore/goimg:$VERSION || exit 1
 fi
 
 echo
 echo Pushing ancientlore/goimg:latest
-docker push ancientlore/goimg:latest
+docker push ancientlore/goimg:latest || exit 1
